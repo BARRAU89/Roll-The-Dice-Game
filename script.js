77251ab61a62591445fabc1c70a2a03a094bb949
Raul Barrios <barrau89@gmail.com>
2023-03-01 13:05:59 -0600

    Roll Dice button functionality coded, game adds value to 'Current' Score and switches between players when dice rolls into 1

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 'use strict';
 
-// ---- Variables to select different elements ------------------------------//
+// ---- Variables to select different elements -----------------------------//
 const score0Element = document.getElementById('score--0');
 const score1Element = document.getElementById('score--1');
+const currentScore0Element = document.getElementById('current--0');
+const currentScore1Element = document.getElementById('current--1');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
-// ---- Defining initial status ------------------------------//
+// ---- Defining initial status --------------------------------------------//
 diceElement.classList.add('hidden');
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+let currentScore = 0;
+let activePlayer = 0; //(0 if player 0 and 1 if player 1)
+const scores = [0, 0];
 
-// ---- Defining initial status ------------------------------//
+// ---- Defining Roll dice functionality -----------------------------------//
+const rollDice = function () {
+  //Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //Discplay the dice image according to random number generated
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+
+  //Check if value === 1; if so, game switch to next player.
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document
+      .querySelector(`.player--${activePlayer === 1 ? 0 : 1}`)
+      .classList.remove('player--active');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
+  }
+};
+
+btnRoll.addEventListener('click', rollDice);
