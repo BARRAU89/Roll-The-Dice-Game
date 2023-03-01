3255f66829ed04e51e7d5b96c4bb6e06e1dea3e0
Raul Barrios <barrau89@gmail.com>
2023-03-01 11:54:50 -0600

    First commit & first elements of the DOM have been changed to initial status of the game

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// ---- Variables to select different elements ------------------------------//
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+// ---- Defining initial status ------------------------------//
+diceElement.classList.add('hidden');
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+
+// ---- Defining initial status ------------------------------//
